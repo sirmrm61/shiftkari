6f5af7a0ced73aae0a5967b59a5d6d94c8f214a4
sirmrm61 <sir.mrm@gmail.com>
2023-09-02 20:29:48 +0330

    0611 - REGISTER USER 20 DEBUG

--- main.py
+++ main.py
@@ -471,6 +471,7 @@ def handle_new_messages(user_id, userName, update):
                     mydb.member_update_chatid('desc', message['text'], chatIdUser)
                     mydb.member_update_chatid('adminChatId', user_id, chatIdUser)
                     # mydb.del_member_chatid(chatid=chatIdUser)
+                    mydb.member_update_chatid('registration_progress', 18, chatIdUser)
                     bot.sendMessage(chatIdUser, msg.messageLib.sorryDenyAdmin.value)
                     bot.sendMessage(chatIdUser, message['text'])
                     bot.sendMessage(chatIdUser, msg.messageLib.selectPropertyForEdit.value,
@@ -546,7 +547,7 @@ def handle_new_messages(user_id, userName, update):
                     bot.sendMessage(message['chat']['id'],
                                     str(msg.messageLib.descDenyAdmin.value))
                     mydb.member_update_chatid('registration_progress', 15, user_id)
-                    mydb.member_update_chatid('registration_progress', 18, spBtn[2])
+                    mydb.member_update_chatid('registration_progress', 15, spBtn[2])
                     mydb.member_update_chatid('adminChatId', user_id, spBtn[2])
                 else:
                     bot.sendMessage(user_id, msg.messageLib.doseVerify.value)
