c81e55cf1ca632f508ad910c183a22ce7348f1d2
sirmrm61 <sir.mrm@gmail.com>
2023-11-26 18:39:22 +0330

    0905.1830 - call any 2 second

--- main.py
+++ main.py
@@ -2246,6 +2246,7 @@ def main(lui=0):
     try:
         while True:
             td = last_ut - ut
+            print(td.total_seconds())
             if last_ut is None or td.total_seconds() > 1:
                 # دریافت تمامی پیام های دریافتی
                 helper.send_shift_to_student(bot=bot)
