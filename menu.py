94dd41acee8d78b2eed29bfeded2ccb97889ad86
sirmrm61 <sir.mrm@gmail.com>
2023-12-12 19:14:23 +0330

    0921.19-20 - em shift

--- menu.py
+++ menu.py
@@ -541,7 +541,7 @@ class keyLib:
         for day in range(startDay, endDay):
             cd = f'{str(year).zfill(4)}-{str(month).zfill(2)}-{str(day).zfill(2)}'
             # print(cd)
-            print(f'{(isEM == 0)}')
+            print(f'isEM={isEM}')
             print(f'{year > int(endDateSelection[0])}')
             print(f'{(year == int(endDateSelection[0]) and month > int(endDateSelection[1]))}')
             print(f'{(year == int(endDateSelection[0]) and month == int(endDateSelection[1]) and day > int(endDateSelection[2]))}')
