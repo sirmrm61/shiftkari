e7f0ba5ab4cf486189b2fb91ba0e66817943609c
sirmrm61 <sir.mrm@gmail.com>
2023-12-12 19:24:41 +0330

    0921.19.2 - em shift

--- menu.py
+++ menu.py
@@ -542,9 +542,7 @@ class keyLib:
             cd = f'{str(year).zfill(4)}-{str(month).zfill(2)}-{str(day).zfill(2)}'
             print(cd)
             # print(f'isEM={isEM}')
-            # print(f'{year > int(endDateSelection[0])}')
-            # print(f'{(year == int(endDateSelection[0]) and month > int(endDateSelection[1]))}')
-            # print(f'{(year == int(endDateSelection[0]) and month == int(endDateSelection[1]) and day > int(endDateSelection[2]))}')
+            print(f'{year > int(endDateSelection[0])}-{(year == int(endDateSelection[0]) and month > int(endDateSelection[1]))}-{(year == int(endDateSelection[0]) and month == int(endDateSelection[1]) and day > int(endDateSelection[2]))}')
             if (isEM == 0) and ((year > int(endDateSelection[0])) or
                                 (year == int(endDateSelection[0]) and month > int(endDateSelection[1])) or
                                 (year == int(endDateSelection[0]) and month == int(endDateSelection[1]) and
