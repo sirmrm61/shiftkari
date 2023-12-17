ccb6f5fb0d1678229e4fb543aa646da669ac6759
sirmrm61 <sir.mrm@gmail.com>
2023-12-17 09:04:58 +0330

    0925.3 - license

--- db/mysqlconnector.py
+++ db/mysqlconnector.py
@@ -1047,6 +1047,7 @@ VALUES({idShift},\'{dateShift}\',\'{requster}\',0,{sendedForCreator},{idDetailSh
     def removeReq(self, idReq):
         sqlQuery = f'''delete from `botshiftkari`.`activity_license` where `id_activity_license` = {idReq} '''
         mydb = self.connector()
+        mydb.autocommit = True
         myCursor = mydb.cursor()
         myCursor.execute(sqlQuery)
         return None
